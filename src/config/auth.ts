0c0957c5e4e16781846b7be09e7e9be2aac112dd
Marcos <marcos@castrobrothers.com.br>
2025-12-30 23:19:08 -0300

    fix: update google api key

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,4 +1,4 @@
 
 export const GOOGLE_CLIENT_ID = "303278738958-tho4efhu3nlcb58ihfppir3o4ughh8gk.apps.googleusercontent.com";
-export const GOOGLE_API_KEY = "AIzaSyD8x-xKBHy-C3NkBhqh_HVJDlTsWUnba4A";
+export const GOOGLE_API_KEY = "AIzaSyDF5MX2aG1QYZJap1-XTdHcReM1Jk5T6Ew";
 export const SCOPES = "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets";
